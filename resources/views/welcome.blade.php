052c889b0bfce07f1e8939bd10b65a165ce36850
ravilushqa <galaktionov.r@gmail.com>
2018-05-06 20:31:51 +0300

    remove hardcode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Task Manager
+                {{ config('app.name', 'Laravel') }}
             </div>
 
             <p>Final php project on Hexlet</p>
