1988097b97bfcaabcfbd9596f8cf5e6fea97bd82
ravilushqa <galaktionov.r@gmail.com>
2018-05-13 00:24:32 +0300

    user task relations

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function createdTasks()
+    {
+        return $this->hasMany(Task::class. 'creator_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'assigned_id');
+    }
 }
