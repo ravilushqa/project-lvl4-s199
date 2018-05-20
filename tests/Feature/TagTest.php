7089076102400c6b397818573da3717a99cdefa3
Ravil Galaktionov <git@web-artisan.ru>
2018-05-20 12:35:42 +0300

    psr

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class tagesTest extends TestCase
+class TagTest extends TestCase
 {
     use RefreshDatabase;
 
