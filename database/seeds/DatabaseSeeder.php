f05ce68bc1ea495a0e82d4fd300822148b486b2b
ravilushqa <galaktionov.r@gmail.com>
2018-05-21 23:58:16 +0300

    add tag seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(TaskStatusSeeder::class);
+         $this->call(TagSeeder::class);
     }
 }
---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $names  = [
+            'bug',
+            'feature'
+        ];
+
+        foreach ($names as $name) {
+            $taskStatus = new \App\Tag();
+            $taskStatus->name = $name;
+            $taskStatus->save();
+        }
+    }
+}
